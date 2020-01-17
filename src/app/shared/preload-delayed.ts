30e3399c68a79718c921a958063b83fea6cef069
Dave <dave@webdave.de>
2020-01-17 15:24:36 +0100

    foo

--- src/app/shared/preload-delayed.ts
+++ src/app/shared/preload-delayed.ts
@@ -1,7 +1,9 @@
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { delay, flatMap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class PreloadDelayed implements PreloadingStrategy {
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
     if (route.data !== undefined) {
