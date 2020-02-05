3e63cdaae84765cc4c65e2cd50a800bd4adb2999
Dave <dave@webdave.de>
2020-02-05 15:21:03 +0100

    pipe

--- src/app/books/shared/pages.pipe.spec.ts
+++ src/app/books/shared/pages.pipe.spec.ts
@@ -5,4 +5,12 @@ describe('PagesPipe', () => {
     const pipe = new PagesPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return "4: Affen"', () => {
+    const pipe = new PagesPipe();
+    expect(pipe.transform(4, 'Affen')).toBeTruthy('4: Affen');
+  });
+  it('should return "42: Hurz"', () => {
+    const pipe = new PagesPipe();
+    expect(pipe.transform(42)).toBeTruthy('42: Hurz');
+  });
 });
