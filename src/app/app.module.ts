857e59123b54b563452a8c5c79a9db44233501bb
Dave <dave@webdave.de>
2020-02-06 15:26:17 +0100

    ng add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { TopNavComponent } from './top-nav/top-nav.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorService } from './error.service';
 import { PreloadDelayed } from './shared/preload-delayed';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, TopNavComponent, WelcomeComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
